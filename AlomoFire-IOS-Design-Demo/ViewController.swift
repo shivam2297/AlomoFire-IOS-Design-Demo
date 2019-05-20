92d80b609945d817502b2b5ece42b37e1d751002
Shivam Bansal <shivam.bansal@daffodilsw.com>
2019-05-20 12:06:23 +0530

    Initial Commit

---
+++ AlomoFire-IOS-Design-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlomoFire-IOS-Design-Demo
+//
+//  Created by Daffolspmac-67 on 20/05/19.
+//  Copyright © 2019 Daffodil Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
